279d5a166f5de6bbe97faa8864c66e51703386e9
pochoa1975 <pochoa@uea.edu.ec>
2024-08-28 21:06:02 -0500

    Versión 1

---
+++ Semana_9/arreglos.py
@@ -0,0 +1,55 @@
+# Arreglo unidimensional
+# Contiene elementos=9, índice máximo=8
+arreglo_unidim=[1,2,3,4,5,6,7,8,9]
+
+# Arreglo bidimensional 2x5
+arreglo_bidim=[
+    [1,2,3,4,5],
+    [6,7,8,9, 10]
+]
+
+# Arreglo tridimensional 2x3x2
+arreglo_tridim=[
+    [
+        [1, 2],
+        [3, 4],
+        [5, 6]
+    ],
+    [
+        [11, 12],
+        [13, 14],
+        [15, 16]
+    ]
+]
+
+# Arreglo multidimensional 2x3x2x4 (cuatro dimensiones)
+arreglo_multidim=[
+    [
+        [
+            [11, 12, 13, 14],
+            [15, 16, 17, 18]
+        ],
+        [
+            [21, 22, 23, 24],
+            [25, 26, 27, 28]
+        ],
+        [
+            [31, 32, 33, 34],
+            [35, 36, 37, 38]
+        ]
+    ],
+    [
+        [
+            [111, 112, 113, 114],
+            [115, 116, 117, 118]
+        ],
+        [
+            [221, 222, 223, 224],
+            [225, 226, 227, 228]
+        ],
+        [
+            [331, 332, 333, 334],
+            [335, 336, 337, 338]
+        ]
+    ]
+]
